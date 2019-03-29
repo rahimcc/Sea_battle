f6bfc1c0f36deb17d18603db915dfc5d1c7c7044
rahimcc <sharifov.rahimcc@gmail.com>
2019-03-29 23:34:38 +0400

    Update V1.1

---
+++ Battle/src/Ship.java
@@ -0,0 +1,16 @@
+public class Ship {
+    String sName;
+    int nSize;
+    int nShip[];
+
+
+    public Ship(String sName , int nSize ){
+        this.sName=sName;
+        this.nSize=nSize;
+        nShip=new int[nSize];
+    }
+
+
+
+
+}
