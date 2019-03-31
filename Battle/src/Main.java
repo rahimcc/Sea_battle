013858f4c7890d46f169c33e3448bf89ffaa68ae
rahimcc <sharifov.rahimcc@gmail.com>
2019-03-31 20:57:12 +0400

    customizing map

--- Battle/src/Main.java
+++ Battle/src/Main.java
@@ -1,34 +1,13 @@
 
 
+
 public class Main {
 
-    public static void printGrid (char[] letters, int[][] grid ){
-        System.out.print("\t");
-
-        for (int i=0;i<grid[0].length;i++){
-            System.out.print(Character.toUpperCase(letters[i])+"  ");
-        }
-        System.out.println();
-
-        
-        for (int i=0;i<grid.length;i++){
-            System.out.print(" ");
-            System.out.print(i);
-            for (int j=0;j<grid[0].length;j++){
-                    System.out.print("*  ");
-                }
-                System.out.println();
-            }
-        }
-
-
-
     public static void main (String[] args){
+        Map UserMap = new Map("User");
+        Map CompMap = new Map("Computer");
 
-        int[][] multi= new int[8][8];
-        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-
-
-        printGrid(alphabet,multi);
+        UserMap.printGrid();
+        CompMap.printGrid();
     }
 }
